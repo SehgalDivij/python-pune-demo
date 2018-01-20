18f28c24c2ab74eb600e33fdff0bd6bd50ac5f68
TheSehgal <divij@flytbase.com>
2018-01-21 00:42:22 +0530

    Added Simple Test Case

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from rest_framework.test import APITestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+
+class IdentityTest(APITestCase):
+    def test_whoami(self):
+        """
+        Check if I am identified right..
+        """
+        url = reverse('identify_me')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'name': 'Divij Sehgal', 'occupation': 'Programming', 'reason': 'It\'s fun', 'age':22})
